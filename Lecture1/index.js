6f146ec0f7ef6723805acc87507c14a2f7714440
amir-bacha <ab963068@gmail.com>
2026-07-26 15:59:56 +0500

    cover req and res object

--- Lecture1/index.js
+++ Lecture1/index.js
@@ -1,12 +1,9 @@
- const express=require('express');
- const app=express();
- app.get('/',(req,res)=>{
-    res.end('Hello world');
- })
- app.get('/user/:id',(req,res)=>{
-    console.log(req.params.id);
-    res.end('Hello user');
- })
- app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
- })
+const express=require('express');
+const app=express();
+app.get('/search',(req,res)=>{
+    console.log(req.query.name);
+    res.end('Hello world this is query parameter')
+});
+app.listen(3000,()=>{
+    console.log("Running at 3000");
+})
